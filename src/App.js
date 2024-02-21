245a90ae7df2d24734f854f1ce494cd135628882
Dr1lla <151372221+Dr1lla@users.noreply.github.com>
2024-02-21 21:53:01 +0200

    first lesson is done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Characters from "./components/lesson-1/Rick&Morty/characters";
+
+
+
+const App = () => {
+    return (
+        <div>
+           <Characters/>
+        </div>
+    );
+};
+
+export default App;
